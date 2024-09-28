75e9d39e0ebcc5b82cbf31f436e323e79684b244
storm22259 <akademi59222@gmail.com>
2024-09-28 15:49:28 -0400

    fix: server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const path = require('path');   // Import the path module
 const app = express();
 
-app.use(express.static('.'));
-app.use('/duck-race', express.static('.'));
+app.use(express.static('.'));   // Serve static files from the current directory
 
+// Serve main.html when the route is '/'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'files', 'main.html'));
-    // Depending on your file structure, you might not need 'files' in the path
 });
 
 // Serve source.html when the route is '/source'
 app.get('/source', (req, res) => {
     res.sendFile(path.join(__dirname, 'files', 'source.html'));
-    // Depending on your file structure, you might not need 'files' in the path
 });
 
 app.listen(3000, () => console.log('server started at 3000'));
